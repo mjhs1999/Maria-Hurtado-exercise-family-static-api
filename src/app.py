d8d2639edb62a9fa1e36324bbc493347edd36fc8
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2023-12-17 15:37:12 +0000

    Finish I have three members

--- src/app.py
+++ src/app.py
@@ -1,44 +1,47 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+#Get a memeber
+@app.route('/member/<int:id>', methods=['GET'])
+def get_members(id):
+    members = jackson_family.get_member(id)
+    return jsonify(members), 200
 
-    return jsonify(response_body), 200
+@app.route('/create/member', methods=['POST'])
+def add_member():
+    member = request.json
+    jackson_family.add_member(member)
+    if not member:
+         return jsonify({"error":"No family member found" }),400
+    return jsonify("family memeber added"), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    return jsonify({"done" : "true"}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
